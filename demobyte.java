8e2b78534e90adeac85b8d9f2fa81301890085e1
Sathish <psathishsivam@gmail.com>
2023-09-14 19:07:02 +0530

    Add files via upload

---
+++ demobyte.java
@@ -0,0 +1,17 @@
+class demo
+ {
+    demo()
+	{
+	  long x=21474836474324l;
+	  System.out.println(x);
+	}
+ }
+
+
+class demobyte
+ {
+    public static void main(String das[])
+	{
+	   demo obj=new demo();
+	}
+ }
