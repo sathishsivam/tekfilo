598556c8b8e509fbe5041594c9d7788099667e22
Sathish <psathishsivam@gmail.com>
2023-09-14 18:30:36 +0530

    Add files via upload

---
+++ samplemethod.java
@@ -0,0 +1,16 @@
+
+class student
+ {
+    public void getstudent()
+	{
+	   System.out.println("Am from student class");
+	}
+ }
+
+class samplemethod
+ {   
+    public static void main(String asd[])
+	{
+	  new student().getstudent();
+	}
+ }
