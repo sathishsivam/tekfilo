c55bf21af82c2fdcd140fba911847f8f987651f2
Sathish <psathishsivam@gmail.com>
2023-09-29 16:41:36 +0530

    Add files via upload

---
+++ demorel1.java
@@ -0,0 +1,24 @@
+class sample
+ {
+    int x=122;
+    sample()
+	{
+	  if(x%2==0)
+	   {
+	     System.out.println("Even number");
+	   }
+	  else
+	   {
+	     System.out.println("Odd number");
+	   }
+
+	}
+ }
+
+class demorel1
+ {
+    public static void main(String asd[])
+	{
+	  new sample();
+	}
+ }
