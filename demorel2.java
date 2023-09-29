e198e233f3fe91a19a9f94f3c08046fc0bc0a044
Sathish <psathishsivam@gmail.com>
2023-09-29 16:48:53 +0530

    Add files via upload

---
+++ demorel2.java
@@ -0,0 +1,34 @@
+class sample
+ {
+    sample()
+	{
+	   int marks=75;
+	
+	   if(marks<40)
+            {
+	      System.out.println("Re appear");
+	    }
+	   else if((marks>=40)&&(marks<60))
+	    {
+	      System.out.println("Pass");
+	    }
+	  else if((marks>60) &&(marks<80))
+	   {
+	      System.out.println("Agrade");
+	   }
+	  else{
+		
+		}
+
+		
+
+	}
+ }
+
+class demorel2
+ {
+    public static void main(String asd[])
+	{
+	   sample obj=new sample();
+	}
+ }
