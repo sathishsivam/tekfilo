d0203cde1064452da3af13b1e730850cb2cc0657
Sathish <psathishsivam@gmail.com>
2023-10-02 11:18:47 +0530

    Add files via upload

---
+++ demolog.java
@@ -0,0 +1,26 @@
+class sample
+ {
+    int x=10,y=23,z=12;
+    sample()
+	{
+	  System.out.println(((y>x) && (y>z)) ||(z>x));  
+	}
+ }
+
+class demolog
+ {
+   public static void main(String asd[])
+	{
+	   sample obj=new sample();
+	}
+ }
+
+	/*
+
+	  and   &&
+
+	  or 	||
+	
+	  not   !
+	*/
+
