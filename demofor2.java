14042046c9c9eb59534d3e0c0bd46c6e8ed1eead
Sathish <psathishsivam@gmail.com>
2023-10-02 15:18:49 +0530

    Add files via upload

---
+++ demofor2.java
@@ -0,0 +1,71 @@
+class sample
+ {
+    sample()
+	{
+	   for(int i=0;i<10;i++)	// 0 1 2 
+	      {
+	       for(int j=0;j<i+1;j++)  // 0 1 2 3 4
+		{
+	          System.out.print("* ");
+		}
+	 	System.out.println("");
+	      }
+
+	}
+ }
+
+
+class demofor2
+ {
+    public static void main(String asd[])
+	{
+	   new sample();
+	}
+ }
+
+
+
+/*
+if()
+ {
+ }
+
+
+if()
+ {
+ }
+else
+ {
+}
+
+
+
+
+while()
+ {
+ }
+
+
+			method()
+			{
+				if()
+				 {
+  
+				 }
+				else if()
+				 {
+				 }
+				else
+				 {
+
+				 }
+			}
+
+			for(initialization;condition;inr/dcr)
+			 {
+			   statement(s);
+ 			}
+
+
+*/			
+	
