f6318483fd33a01c416754a33eaec2805fefa414
Sathish <psathishsivam@gmail.com>
2023-10-02 15:18:26 +0530

    Add files via upload

---
+++ demofor.java
@@ -0,0 +1,70 @@
+class sample
+ {
+    sample()
+	{
+	   for(int i=0;i<10;i++)
+	      {
+	          System.out.println(i);
+	      }
+
+	  System.out.println("exit from loop");
+	
+
+	}
+ }
+
+
+class demofor
+ {
+    public static void main(String asd[])
+	{
+	   new sample();
+	}
+ }
+
+
+
+/*
+if()
+ {
+ }
+
+
+if()
+ {
+ }
+else
+ {
+}
+
+
+
+
+while()
+ {
+ }
+
+
+			method()
+			{
+				if()
+				 {
+  
+				 }
+				else if()
+				 {
+				 }
+				else
+				 {
+
+				 }
+			}
+
+			for(initialization;condition;inr/dcr)
+			 {
+			   statement(s);
+ 			}
+
+
+*/			
+	
---
+++ demofor1.java
@@ -0,0 +1,71 @@
+class sample
+ {
+    sample()
+	{
+	   for(int i=0;i<3;i++)	// 0 1 2 
+	      {
+	       for(int j=0;j<5;j++)  // 0 1 2 3 4
+		{
+	          System.out.print(j);
+		}
+		System.out.println("");
+	      }
+
+	}
+ }
+
+
+class demofor1
+ {
+    public static void main(String asd[])
+	{
+	   new sample();
+	}
+ }
+
+
+
+/*
+if()
+ {
+ }
+
+
+if()
+ {
+ }
+else
+ {
+}
+
+
+
+
+while()
+ {
+ }
+
+
+			method()
+			{
+				if()
+				 {
+  
+				 }
+				else if()
+				 {
+				 }
+				else
+				 {
+
+				 }
+			}
+
+			for(initialization;condition;inr/dcr)
+			 {
+			   statement(s);
+ 			}
+
+
+*/			
+	
