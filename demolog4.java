d3370a87d364aca1008e876b30fa4fd1303dbed2
Sathish <psathishsivam@gmail.com>
2023-10-02 11:43:59 +0530

    Add files via upload

---
+++ demolog4.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+class sample
+ {
+   String ch="yes";
+   Scanner s=new Scanner(System.in);
+    sample()
+	{
+	  do
+	    {
+		System.out.println("Enter the product");
+		String p1=s.nextLine();
+	        ch=s.nextLine();
+	    }while((ch.equals("yes")));   
+	}
+
+ }
+
+
+class demolog4
+ {
+    public static void main(String asd[])
+	{
+	   new sample();
+	}
+ }
