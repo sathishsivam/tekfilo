002891bb11a8cff8de2fe5b2f91d9967eb04863e
Sathish <psathishsivam@gmail.com>
2023-10-02 11:44:23 +0530

    Add files via upload

---
+++ demolog2.java
@@ -0,0 +1,22 @@
+
+class sample
+ {
+   String ch="yes";
+    sample()
+	{
+	  while(ch.equals("yes"))
+	    {
+		 System.out.println("welcome to i18n");	
+	    }   
+	}
+
+ }
+
+
+class demolog2
+ {
+    public static void main(String asd[])
+	{
+	   new sample();
+	}
+ }
